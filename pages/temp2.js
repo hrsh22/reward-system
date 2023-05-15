93ceffc0d7171a03aa23add29abc29fd35f321ab
Harsh Gupta <harsh.gupta2211@gmail.com>
2023-05-15 22:38:14 +0530

    update homepage

---
+++ pages/temp2.js
@@ -0,0 +1,86 @@
+<div className="max-w-[1248px] mx-auto flex flex-wrap justify-center">
+  <div className="w-full md:w-6/12 lg:w-4/12 px-4">
+    <div className="relative flex flex-col mt-4">
+      <div className="px-4 py-5 mb-1 flex-auto">
+        <div className="text-slate-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-5 shadow-lg rounded-full bg-white">
+          <i className="fa-solid fa-right-left text-4xl"></i>
+        </div>
+        <h6 className="text-3xl mb-1 font-semibold">
+          Seamless Reward Point Exchange
+        </h6>
+        <p className="mb-4 text-lg text-slate-500">
+          Exchange and redeem reward points effortlessly using blockchain technology.
+        </p>
+      </div>
+    </div>
+    <div className="relative flex flex-col min-w-0">
+      <div className="px-4 py-5 flex-auto">
+        <div className="text-slate-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-5 shadow-lg rounded-full bg-white">
+          <i className="fa-solid fa-certificate text-4xl"></i>
+        </div>
+        <h6 className="text-3xl mb-1 font-semibold">
+          NFTs as Exclusive Rewards
+        </h6>
+        <p className="mb-4 text-lg text-slate-500">
+          Quickmint NFTs to create unique and exclusive rewards for your customers.
+        </p>
+      </div>
+    </div>
+    <div className="relative flex flex-col min-w-0">
+      <div className="px-4 py-5 flex-auto">
+        <div className="text-slate-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-5 shadow-lg rounded-full bg-white">
+          <i className="fa-solid fa-circle-plus text-4xl"></i>
+        </div>
+        <h6 className="text-3xl mb-1 font-semibold">
+          Continuous Token Minting
+        </h6>
+        <p className="mb-4 text-lg text-slate-500">
+          Mint additional tokens as rewards to engage and incentivize your customer base.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div className="w-full md:w-6/12 lg:w-4/12 px-4">
+    <div className="relative flex flex-col min-w-0 mt-4">
+      <div className="px-4 py-5 flex-auto">
+        <div className="text-slate-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-5 shadow-lg rounded-full bg-white">
+          <i className="fa-solid fa-puzzle-piece text-4xl"></i>
+        </div>
+        <h6 className="text-3xl mb-1 font-semibold">
+          Simplified Integration
+        </h6>
+        <p className="mb-4 text-lg text-slate-500">
+          Easy integration with your existing systems, no coding or blockchain knowledge required.
+        </p>
+      </div>
+    </div>
+    <div className="relative flex flex-col min-w-0">
+      <div className="px-4 py-5 flex-auto">
+      <div className="text-slate-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-5 shadow-lg rounded-full bg-white">
+    <i className="fa-solid fa-medal text-4xl"></i>
+  </div>
+  <h6 className="text-3xl mb-1 font-semibold">
+    Branded Tokens
+  </h6>
+  <p className="mb-4 text-lg text-slate-500">
+    Create your own branded tokens as loyalty points or rewards within your ecosystem.
+  </p>
+</div>
+</div>
+<div className="relative flex flex-col min-w-0">
+<div className="px-4 py-5 flex-auto">
+  <div className="text-slate-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-5 shadow-lg rounded-full bg-white">
+    <i className="fa-solid fa-fire text-4xl"></i>
+  </div>
+  <h6 className="text-3xl mb-1 font-semibold">
+    Controlled Token Burning
+  </h6>
+  <p className="mb-4 text-lg text-slate-500">
+    Retire or remove tokens from circulation when needed for effective token management.
+  </p>
+</div>
+</div>
+</div>
+</div>
+
+
